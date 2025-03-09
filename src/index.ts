d8fb4d3fa1836c0777df4a9546f0287182a3bb85
marcus-sa <work@marcus-sa.dev>
2025-03-09 18:16:57 +0100

    fix: external api exports deepkit doesn't support named re-exports https://github.com/deepkit/deepkit-framework/issues/634
    Signed-off-by: marcus-sa <work@marcus-sa.dev>

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,13 @@ export { restate } from './decorator.js';
 export { RestateModule } from './module.js';
 export { RestateAdminClient } from './admin-client.js';
 export { RestateHttpClient, RestateMemoryClient } from './client.js';
+export { provideRestateObjectProxy, provideRestateServiceProxy, createClassProxy } from './utils/type.js';
 export {
   RestateConfig,
   RestateKafkaConfig,
   RestateServerConfig,
 } from './config.js';
-export {
-  RestateObjectContext,
-  RestateServiceContext,
-  RestateSagaContext,
-  restateSagaContextType,
-  restateObjectContextType,
-  restateServiceContextType,
-} from './context.js';
-export {
-  createClassProxy,
-  getRestateSagaMetadata,
-  getRestateServiceMetadata,
-  getRestateObjectMetadata,
-  provideRestateServiceProxy,
-  provideRestateObjectProxy,
-} from './utils/type.js';
-export { RestateService, RestateObject, RestateSaga } from './types.js';
+export * from './context.js';
+export * from './types.js';
 export * from './event/index.js';
 export * from './saga/index.js';
